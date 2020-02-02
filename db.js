af83d212c320af59eba44772852b4d1455d8c94b
Vinod <vinod@vinod.co>
2020-02-02 07:40:41 +0530

    🇮🇳

---
+++ db.js
@@ -0,0 +1,7 @@
+module.exports = () => ({
+    products: require('./data/products.json'),
+    categories: require('./data/categories.json'),
+    brands: require('./data/brands.json'),
+    customers: require('./data/customers.json'),
+    orders: require('./data/orders.json')
+});
